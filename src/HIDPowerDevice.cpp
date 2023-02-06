ed6c943f5e8f2c871bb35bd81fc16684008d9806
B0urdonnais <124383522+B0urdonnais@users.noreply.github.com>
2023-02-06 23:05:37 +0100

    Update HIDPowerDevice.cpp
    Added 0x85, HID_PD_CURRENT

--- src/HIDPowerDevice.cpp
+++ src/HIDPowerDevice.cpp
@@ -151,6 +151,13 @@ static const uint8_t _hidReportDescriptor[] PROGMEM = {
     0xB1, 0xA2, //     FEATURE (Data, Variable, Absolute, No Wrap, Linear, No Preferred, No Null Position, Volatile, Bitfield)
     0x09, 0x02, //     USAGE (PresentStatus)
     0xA1, 0x02, //     COLLECTION (Logical)
+  // add current
+    0x85, HID_PD_CURRENT, //     REPORT_ID (21)  copied from PD voltage above
+    0x09, 0x31, //     USAGE (Voltage)  <-- wrong, maybe 31 (see page 21 of usb hid spec)
+    0x81, 0xA3, //     INPUT (Constant, Variable, Absolute, No Wrap, Linear, No Preferred, No Null Position, Bitfield)
+    0x09, 0x31, //     USAGE (Voltage)  <-- wrong, maybe 31 (see page 21 of usb hid spec)
+    0xB1, 0xA3, //     FEATURE (Constant, Variable, Absolute, No Wrap, Linear, No Preferred, No Null Position, Volatile, Bitfield)
+  // /add current
     0x85, HID_PD_PRESENTSTATUS, //       REPORT_ID (7)
     0x05, 0x85, //       USAGE_PAGE (Battery System) =================
     0x09, 0x44, //       USAGE (Charging)
